9609a7ffa14d0276f3b8d322c6c3eb60c074521e
iqpizza6349 <iqpizza6349@gmail.com>
2022-09-17 20:08:43 +0900

    :wrench: :: add configuration about resource web properties

--- src/main/java/kr/hs/ide/nextto/domain/member/entity/Member.java
+++ src/main/java/kr/hs/ide/nextto/domain/member/entity/Member.java
@@ -3,14 +3,19 @@ package kr.hs.ide.nextto.domain.member.entity;
 import kr.hs.ide.nextto.domain.detail.entity.MemberDetail;
 import lombok.*;
 import org.springframework.data.annotation.Id;
+import org.springframework.security.core.GrantedAuthority;
+import org.springframework.security.core.authority.SimpleGrantedAuthority;
+import org.springframework.security.core.userdetails.UserDetails;
 
 import java.time.LocalDate;
+import java.util.Collection;
+import java.util.Collections;
 
 @Getter
 @Builder
 @AllArgsConstructor
 @NoArgsConstructor(access = AccessLevel.PROTECTED)
-public class Member {
+public class Member implements UserDetails {
 
     @Id
     private Long memberId;
@@ -18,4 +23,39 @@ public class Member {
     private LocalDate renewalDate;
 
     private MemberDetail memberDetail;
+
+    @Override
+    public Collection<? extends GrantedAuthority> getAuthorities() {
+        return Collections.singleton(new SimpleGrantedAuthority("PERMISSION"));
+    }
+
+    @Override
+    public String getPassword() {
+        return null;
+    }
+
+    @Override
+    public String getUsername() {
+        return String.valueOf(memberId);
+    }
+
+    @Override
+    public boolean isAccountNonExpired() {
+        return false;
+    }
+
+    @Override
+    public boolean isAccountNonLocked() {
+        return false;
+    }
+
+    @Override
+    public boolean isCredentialsNonExpired() {
+        return false;
+    }
+
+    @Override
+    public boolean isEnabled() {
+        return true;
+    }
 }
